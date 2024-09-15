213ecbc7e3b6f1daedb0f55a5c3bdba5759f0004
Mansoor Khan <mansoorkhan@192.168.1.19>
2024-09-15 21:50:55 +0530

    Image not loading issue check

--- app/awards/page.tsx
+++ app/awards/page.tsx
@@ -8,18 +8,18 @@ export default function Awards() {
 
                 <div className="rounded-lg overflow-hidden shadow-2xl m-10 p-5" style={{border:"10px solid #00897B"}}>
 
-                    <Image className="inline-block" src={"/assets/certificates/pwc.jpg"} width={800} height={450} alt="" />
+                    <Image className="inline-block" src={"./assets/certificates/pwc.jpg"} width={800} height={450} alt="" />
                 </div>
 
                 <div className="rounded-lg overflow-hidden shadow-2xl m-10 p-5" style={{border:"10px solid #00897B"}}>
 
-                    <Image className="inline-block" src={"/assets/certificates/cisco.jpg"} width={800} height={450} alt="" />
+                    <Image className="inline-block" src={"./assets/certificates/cisco.jpg"} width={800} height={450} alt="" />
 
                 </div>
 
                 <div className="rounded-lg overflow-hidden shadow-2xl m-10 p-5" style={{border:"10px solid #00897B"}}>
 
-                    <Image className="inline-block" src={"/assets/certificates/bct.jpg"} width={800} height={450} alt="" />
+                    <Image className="inline-block" src={"./assets/certificates/bct.jpg"} width={800} height={450} alt="" />
 
                 </div>
             </div>
