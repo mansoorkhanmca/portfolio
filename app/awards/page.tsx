b9cb255b8c526f6e013d2e72f67548fd88e01e31
Mansoor Khan <mansoorkhan@192.168.1.21>
2024-09-18 08:58:44 +0530

    Added additional certificates

--- app/awards/page.tsx
+++ app/awards/page.tsx
@@ -8,7 +8,8 @@ export default function Awards() {
 
                 <div className="rounded-lg overflow-hidden shadow-2xl m-10 p-5" style={{border:"10px solid #00897B"}}>
 
-                    <Image className="inline-block" src={"/portfolio/assets/certificates/pwc.jpg"} width={800} height={450} alt="" />
+                    <Image className="inline-block" src={"/portfolio/assets/certificates/cisco-50k.jpg"} width={800} height={450} alt="" />
+
                 </div>
 
                 <div className="rounded-lg overflow-hidden shadow-2xl m-10 p-5" style={{border:"10px solid #00897B"}}>
@@ -19,9 +20,14 @@ export default function Awards() {
 
                 <div className="rounded-lg overflow-hidden shadow-2xl m-10 p-5" style={{border:"10px solid #00897B"}}>
 
+                    <Image className="inline-block" src={"/portfolio/assets/certificates/pwc.jpg"} width={800} height={450} alt="" />
+                </div>
+
+                {/* <div className="rounded-lg overflow-hidden shadow-2xl m-10 p-5" style={{border:"10px solid #00897B"}}>
+
                     <Image className="inline-block" src={"/portfolio/assets/certificates/bct.jpg"} width={800} height={450} alt="" />
 
-                </div>
+                </div> */}
             </div>
         </BodyWrapper>
     );
