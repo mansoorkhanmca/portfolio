0443f181a2f70bb459390e253eaee0bdf5fe3e02
Mansoor Khan <mansoorkhan@192.168.1.17>
2024-10-20 20:02:42 +0530

    Updated Resume and Front page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,21 +5,20 @@ export default function About() {
         <BodyWrapper>
             <div className="block p-4 text-2xl about">
                <p> <span className="font-bold text-teal-600">Full Stack Developer</span> with over 11 years of experience in web development and a deep technical
-                expertise, I specialize in crafting dynamic, responsive web applications. 
-                <br/>My background includes:
+                expertise, I specialize in crafting dynamic, responsive web applications. My expertise spans across both front-end and back-end technologies, allowing me to deliver end-to-end solutions that meet modern web standards.
                 </p><br/>
                 Extensive Experience:
                 <ul className="p-4 text-xl list-disc ml-10 about-points">
                     <li>11+ years in web development with robust technical knowledge.</li>
                     <li>7+ years focused on Single Page Applications (SPA) and backend development.</li>
-                    <li>3+ years of experience in AWS Cloud Infrastructure and Cloud AWS Development.</li>
+                    <li>3+ years of experience in creating AWS Cloud Infrastructure and AWS Cloud Development.</li>
                 </ul>
                 
                 Versatile Technology Stack:
                 <ul className="p-4 text-xl list-disc ml-10 about-points">
                     <li>Proficient in <span className="font-semibold text-red-600">MEAN and MERN</span> stacks, as well as <span className="font-semibold text-blue-600">Java Full Stack</span> technologies.</li>
                     <li>Programming skills in modern frameworks like <span className="font-semibold text-rose-400">Angular 17, React JS 18 and Next JS 14</span>.</li>
-                    <li>Backend experience with <span className="font-semibold text-purple-600">Spring Boot 3, Spring Cloud, Node.js and Epress.js</span>.</li>
+                    <li>Backend experience with <span className="font-semibold text-purple-600">Spring Boot 3, Spring Cloud, Node.js and Express.js</span>.</li>
                     <li>Hands-on experience with AWS Lambda and <span className="font-semibold text-orange-600">Serverless</span> computing.</li>
                 </ul>
                 Advanced Technical Skills:
@@ -38,8 +37,9 @@ export default function About() {
                 DevOps and CI/CD:
                 <ul className="p-4 text-xl list-disc ml-10 about-points">
                     <li>Knowledgeable in CI/CD tools, including <span className="font-semibold text-lime-600">Jenkins Pipelines and GitHub Actions</span>.</li>
-                    <li>Proficient in <span className="font-semibold text-red-600">AWS</span> services and infrastructure management using <span className="font-semibold text-amber-600">Terraform</span>.</li>
-                    <li>Experience in setting up and managing robust <span className="font-semibold text-lime-700">Architectural Design</span>.</li>
+                    <li>Proficient in <span className="font-semibold text-red-600">AWS</span> services and <span className="font-semibold text-amber-600">Cloud Native</span> technologies.</li>
+                    <li>Experience in Cloud infrastructure management using <span className="font-semibold text-blue-600">Terraform</span>.</li>
+                    <li>Knowledge in setting up and managing robust <span className="font-semibold text-purple-600">Architectural Design</span>.</li>
                 </ul>
                 
                 Agile and Remote Collaboration:
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,14 +1,19 @@
 import { ImMail4 } from "react-icons/im";
 import { RxGithubLogo } from "react-icons/rx";
 import { FaLinkedin } from "react-icons/fa6";
+import { FaMobileRetro } from "react-icons/fa6";
 import BodyWrapper from "../components/bodyWrapper";
 
 export default function Contact() {
     return (
         <BodyWrapper>
-            <div className="flex justify-center  items-center p-52 pdng">
+            <div className="flex justify-center  items-center p-24">
                 <table>
                     <tbody>
+                        <tr>
+                            <th><FaMobileRetro className="m-5 text-5xl" /></th>
+                            <th className="text-left text-teal-600 cursor-pointer"><a>+91-9790407040</a></th>
+                        </tr>
                         <tr>
                             <th><ImMail4 className="m-5 text-5xl" /></th>
                             <th className="text-left text-teal-600 cursor-pointer"><a href="mailto:mansoorkhanmca@gmail.com">mansoorkhanmca@gmail.com</a></th>
